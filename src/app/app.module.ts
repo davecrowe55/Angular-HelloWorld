16d709bbf34c55b364dcd56d58135e80c12b9ec5
davecrowe55 <33950559+davecrowe55@users.noreply.github.com>
2021-04-07 11:29:18 +1200

    Component testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { AppComponent } from './app.practice';
 
 @NgModule({
   declarations: [
